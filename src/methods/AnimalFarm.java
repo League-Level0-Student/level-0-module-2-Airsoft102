a8232d7c1fdcda360a12d66389bfd6e757b651f9
League <league@WTS8.attlocal.net>
2018-11-06 18:28:38 -0800

    great
    dice rool magic 8 ball Sort of lottery  ticket

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -7,7 +7,6 @@ import java.applet.AudioClip;
 
 //Level 0
 
-
 import javax.swing.JApplet;
 import javax.swing.JOptionPane;
 
@@ -17,30 +16,30 @@ public class AnimalFarm {
 		/*
 		 * 1. Ask the user which animal they want, then play the sound of that animal.
 		 */
-	String animal= JOptionPane.showInputDialog("choose an animal");
-		/* 2. Make it so that the user can keep entering new animals. */
-	
-if(animal.equals("llama")){
-	playNoise(llamaFile);
-}
-	if(animal.equals("cat")) {
-	playNoise(meowFile);
+		for (int i = 0; i < 7; i++) {
+			String animal = JOptionPane.showInputDialog("choose an animal");
+			/* 2. Make it so that the user can keep entering new animals. */
+
+			if (animal.equals("llama")) {
+				playNoise(llamaFile);
+			}
+			if (animal.equals("cat")) {
+				playNoise(meowFile);
+			}
+			if (animal.equals("cow")) {
+				playNoise(mooFile);
+			}
+			if (animal.equals("duck")) {
+				playNoise(quackFile);
+			}
+			if (animal.equals("dog")) {
+				playNoise(woofFile);
+			}
+		}
+
 	}
-if (animal.equals("cow")) {
-	playNoise(mooFile);
-}
-	if (animal.equals("duck")) {
-		playNoise(quackFile);
-	}
-	if (animal.equals("dog")) {
-		playNoise(woofFile);
-	}
-	
-		
-	}
-		
-		
-		void playMoo() {
+
+	void playMoo() {
 		playNoise(mooFile);
 	}
 
@@ -72,6 +71,6 @@ if (animal.equals("cow")) {
 
 	public static void main(String[] args) {
 		new AnimalFarm();
-	
 
-}}
+	}
+}
--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -1,35 +1,50 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2018
 //    Level 0
 
-
 package random;
 
+import java.nio.channels.ShutdownChannelGroupException;
 import java.util.Random;
+
+import javax.swing.JOptionPane;
 
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+	public static void main(String[] args) {
 
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+		// 2. Make a variable that will hold a random number and put a random number
+		// into this variable using "new Random().nextInt(4)"
+		for (int i = 0; i < 10; i++) {
+			int ran = new Random().nextInt(4);
 
-	// 3. Print out this variable
+			// 3. Print out this variable
+			System.out.println(ran);
+			// 4. Get the user to enter a question for the 8 ball
+			JOptionPane.showInputDialog("put in your thoughts");
+			// 5. If the random number is 0
+			if (ran == 0) {
+				JOptionPane.showMessageDialog(null, "Yes");
+			}
+			// -- tell the user "Yes"
+			else if (ran == 1) {
+				JOptionPane.showMessageDialog(null, "no said Mo");
+			}
+			// 6. If the random number is 1
 
-	// 4. Get the user to enter a question for the 8 ball
+			// -- tell the user "No"
 
-	// 5. If the random number is 0
+			// 7. If the random number is 2
+			else if (ran == 2) {
+				JOptionPane.showMessageDialog(null, "Maybe you should ask Google ");
+			}
+			// -- tell the user "Maybe you should ask Google?"
 
-	// -- tell the user "Yes"
-
-	// 6. If the random number is 1
-
-	// -- tell the user "No"
-
-	// 7. If the random number is 2
-
-	// -- tell the user "Maybe you should ask Google?"
-
-	// 8. If the random number is 3
-
-	// -- write your own answer
-
+			// 8. If the random number is 3
+			else if (ran == 3) {
+				JOptionPane.showConfirmDialog(null, "sip on straight chlorine");
+			}
+			// -- write your own answer
+		}
+	}
 }
